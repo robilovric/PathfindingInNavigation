24b27c8bf398854652121e4d0140ababc8dd741e
robilovric <robilovric7@gmail.com>
2022-08-24 01:26:37 +0200

    Initial project setup, ready for algorithm implementation

---
+++ MyDijkstra.py
@@ -0,0 +1,5 @@
+import osmnx as ox
+import networkx as nx
+
+def PrepareMinimalGraph(startPos, endPos):
+    pass
---
+++ main.py
@@ -0,0 +1,217 @@
+import osmnx as ox
+import networkx as nx
+import geopandas
+import folium
+from folium.plugins import MousePosition, Geocoder
+import io
+import sys
+from PyQt5 import QtWebEngineWidgets, QtWidgets
+
+class Window(QtWidgets.QMainWindow):
+    startMarkerCount = 0
+    destMarkerCount = 0
+    startPoint = []
+    destinationPoint = []
+
+    def __init__(self):
+        super().__init__()
+        self.InitWindow()
+
+    def InitWindow(self):
+        self.setWindowTitle(self.tr("DIJKSTRA FOR NAVIGATION"))
+        self.setFixedSize(1500, 800)
+        self.ArrangeUI()
+
+    def ArrangeUI(self):
+        self.startLatLabel = QtWidgets.QLabel(self.tr("Start latitude:"))
+        self.startLonLabel = QtWidgets.QLabel(self.tr("Start longitude:"))
+        self.startLatEdit = QtWidgets.QLineEdit()
+        self.startLonEdit = QtWidgets.QLineEdit()
+        self.destLatLabel = QtWidgets.QLabel(self.tr("Destination latitude:"))
+        self.destLonLabel = QtWidgets.QLabel(self.tr("Destination longitude:"))
+        self.destLatEdit = QtWidgets.QLineEdit()
+        self.destLonEdit = QtWidgets.QLineEdit()
+        self.addStartMarkerBtn = QtWidgets.QPushButton(self.tr("Add start location"))
+        self.addDestMarkerBtn = QtWidgets.QPushButton(self.tr("Add destination location"))
+        self.findPathButton = QtWidgets.QPushButton(self.tr("Visualize graph"))
+        self.clearButton = QtWidgets.QPushButton(self.tr("Clear map"))
+
+        self.addStartMarkerBtn.clicked.connect(self.AddStartButtonClicked)
+        self.addDestMarkerBtn.clicked.connect(self.AddDestButtonClicked)
+        self.findPathButton.clicked.connect(self.get_graph)
+        self.clearButton.clicked.connect(self.ClearMap)
+
+        self.view = QtWebEngineWidgets.QWebEngineView()
+        self.view.setContentsMargins(5, 5, 5, 5)
+
+        centralWidget = QtWidgets.QWidget()
+        self.setCentralWidget(centralWidget)
+        mainLayout = QtWidgets.QHBoxLayout(centralWidget)
+
+        controlContainer = QtWidgets.QWidget()
+        gridLayout = QtWidgets.QGridLayout(controlContainer)
+        gridLayout.addWidget(self.startLatLabel, 0, 0)
+        gridLayout.addWidget(self.startLatEdit, 0, 1)
+        gridLayout.addWidget(self.startLonLabel, 1, 0)
+        gridLayout.addWidget(self.startLonEdit, 1, 1)
+        gridLayout.addWidget(self.addStartMarkerBtn, 2, 0, 1, 2)
+        gridLayout.addWidget(self.destLatLabel, 3, 0)
+        gridLayout.addWidget(self.destLatEdit, 3, 1)
+        gridLayout.addWidget(self.destLonLabel, 4, 0)
+        gridLayout.addWidget(self.destLonEdit, 4, 1)
+        gridLayout.addWidget(self.addDestMarkerBtn, 5, 0, 1, 2)
+        gridLayout.addWidget(self.findPathButton, 6, 0, 1, 2)
+        gridLayout.addWidget(self.clearButton, 7, 0, 1, 2)
+        mainLayout.addWidget(controlContainer)
+        mainLayout.addWidget(self.view, stretch=1)
+
+        self.m = folium.Map(
+            location=[43.5347, 16.3600],
+            tiles="OpenStreetMap",
+            zoom_start=13
+        )
+        self.m.add_child(folium.LatLngPopup())
+
+        MousePosition(
+            position="bottomleft",
+            separator=" | ",
+            empty_string="NaN",
+            num_digits=7,
+            prefix="Coordinates:",
+        ).add_to(self.m)
+
+        Geocoder().add_to(self.m)
+
+
+        data = io.BytesIO()
+        self.m.save(data, close_file=False)
+        self.view.setHtml(data.getvalue().decode())
+
+    def AddStartButtonClicked(self):
+        if(Window.startMarkerCount==1):
+            msgBox = QtWidgets.QMessageBox()
+            msgBox.setIcon(QtWidgets.QMessageBox.Warning)
+            msgBox.setText("You can have only one starting point!\nUse Clear map button to be able to add a new point.")
+            msgBox.setWindowTitle("WARNING")
+            msgBox.setStandardButtons(QtWidgets.QMessageBox.Ok | QtWidgets.QMessageBox.Cancel)
+            msgBox.exec()
+            return
+        lat = self.startLatEdit.text()
+        lon = self.startLonEdit.text()
+        Window.startMarkerCount += 1
+        Window.startPoint.append(float(lat))
+        Window.startPoint.append(float(lon))
+        self.UpdateMap(lat, lon, "blue", "fa-rocket", "Starting point!")
+
+    def AddDestButtonClicked(self):
+        if Window.destMarkerCount==1:
+            msgBox = QtWidgets.QMessageBox()
+            msgBox.setIcon(QtWidgets.QMessageBox.Warning)
+            msgBox.setText("You can have only one destination!\nUse Clear map button to be able to add a new point.")
+            msgBox.setWindowTitle("WARNING")
+            msgBox.setStandardButtons(QtWidgets.QMessageBox.Ok | QtWidgets.QMessageBox.Cancel)
+            msgBox.exec()
+            return
+        lat = self.destLatEdit.text()
+        lon = self.destLonEdit.text()
+        Window.destMarkerCount += 1
+        Window.destinationPoint.append(float(lat))
+        Window.destinationPoint.append(float(lon))
+        self.UpdateMap(lat, lon, "red", "fa-star", "Destination point!")
+
+    def UpdateMap(self, lat, lon, color, icon, popup):
+        self.m.location = [float(lat), float(lon)]
+        folium.Marker(
+            location=[float(lat), float(lon)],
+            icon=folium.Icon(color=color, icon=icon, prefix="fa"),
+            popup=popup
+        ).add_to(self.m)
+        data = io.BytesIO()
+        self.m.save(data, close_file=False)
+        self.view.setHtml(data.getvalue().decode())
+        self.statusBar().showMessage("Marker added!")
+
+    def ClearMap(self):
+        if len(Window.startPoint) == 2:
+            self.m = folium.Map(
+                location=[Window.startPoint[0], Window.startPoint[1]],
+                tiles="OpenStreetMap",
+                zoom_start=13
+            )
+            self.m.add_child(folium.LatLngPopup())
+
+            MousePosition(
+                position="bottomleft",
+                separator=" | ",
+                empty_string="NaN",
+                num_digits=7,
+                prefix="Coordinates:",
+            ).add_to(self.m)
+
+            Geocoder().add_to(self.m)
+
+            data = io.BytesIO()
+            self.m.save(data, close_file=False)
+            self.view.setHtml(data.getvalue().decode())
+
+            Window.startMarkerCount = 0
+            Window.destMarkerCount = 0
+            Window.startPoint = []
+            Window.estinationPoint = []
+
+
+    def get_graph(self):
+        if(Window.startPoint[0] > Window.destinationPoint[0]
+                and Window.startPoint[1] > Window.destinationPoint[1]):
+            myG = ox.graph_from_bbox(
+                Window.startPoint[0], Window.destinationPoint[0],
+                Window.startPoint[1], Window.destinationPoint[1],
+                network_type="drive",
+                truncate_by_edge=True,
+                clean_periphery=False
+            )
+            ox.plot_graph(myG)
+        elif(Window.startPoint[0] > Window.destinationPoint[0]
+                and Window.startPoint[1] < Window.destinationPoint[1]):
+            myG = ox.graph_from_bbox(
+                Window.startPoint[0], Window.destinationPoint[0],
+                Window.destinationPoint[1], Window.startPoint[1],
+                network_type="drive",
+                truncate_by_edge=True,
+                clean_periphery=False
+            )
+            ox.plot_graph(myG)
+        elif(Window.startPoint[0] < Window.destinationPoint[0]
+                and Window.startPoint[1] > Window.destinationPoint[1]):
+            myG = ox.graph_from_bbox(
+                Window.destinationPoint[0], Window.startPoint[0],
+                Window.startPoint[1], Window.destinationPoint[1],
+                network_type="all",
+                truncate_by_edge=True,
+                clean_periphery=False
+            )
+            ox.plot_graph(myG)
+        else:
+            myG = ox.graph_from_bbox(
+                Window.destinationPoint[0], Window.startPoint[0],
+                Window.destinationPoint[1], Window.startPoint[1],
+                network_type="all",
+                truncate_by_edge=True,
+                clean_periphery=False
+            )
+            ox.plot_graph(myG)
+
+if __name__ == "__main__":
+    App = QtWidgets.QApplication(sys.argv)
+    window = Window()
+    window.show()
+    sys.exit(App.exec())
+
+
+
+
+
+
+
+
+
